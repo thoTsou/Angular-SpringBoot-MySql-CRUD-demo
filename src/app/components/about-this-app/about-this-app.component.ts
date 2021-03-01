b812738cd8ae3fb511d0de550f7171b642f10904
thoTsou <tsoufis.thodoris@gmail.com>
2021-03-01 17:26:18 +0200

    first Commit

---
+++ src/app/components/about-this-app/about-this-app.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-about-this-app',
+  templateUrl: './about-this-app.component.html',
+  styleUrls: ['./about-this-app.component.css']
+})
+export class AboutThisAppComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
